2c74c2d3a4d49b51d907339668af94758c9abbea
Emre Karaman <emrkaraman@gmail.com>
2019-06-20 09:56:50 +0200

    Update functions.jl

--- functions.jl
+++ functions.jl
@@ -208,3 +208,129 @@ function outputControl_shaoLei(sum2pq,onScreen,iter,these2Keep,tempBetaMat,μ,co
         end
     end
 end
+
+function prepRegionData(snpInfo,chrs,genoTrain,fixedRegSize)
+    accRegion = 0
+    accRegionVec = [0]
+    SNPgroups = []
+#    mapData = readtable(pwd()"/$mapFile", header=false)
+    ###only for our map file
+    mapData = readtable("$snpInfo", header=false, separator=' ')
+    if size(mapData,2)<5
+        mapData = hcat(collect(1:size(mapData,1)),mapData,makeunique=true)
+    end
+    headMap = [:row, :snpID, :snpOrder ,:chrID, :pos]
+    rename!(mapData , names(mapData), headMap)
+    print(mapData[1:5,:])
+    mapData[:snpID] = ["M$i" for i in 1:size(mapData,1)] #to convert original IDs like "HAPMAP43437-BTA-101873"
+    print(mapData[1:10,:])
+    ###
+    mapData = mapData[mapData[:chrID] .<= chrs,:]
+    # if first col in genoTrain is ID
+    # I find cols that are in mapData (<chrs), and select those
+    usedLoci = intersect(names(genoTrain),Symbol.(mapData[:snpID]))
+    mapData = mapData[[find(usedLoci[i].==Symbol.(mapData[:snpID]))[] for i in 1:length(usedLoci)],:] #trim map data
+    genoX = genoTrain[vcat(Symbol("ID"),usedLoci)]    #trim genoData
+#     genoX = genoTrain[[1; [find(i -> i == j, names(genoTrain))[] for j in [Symbol(mapData[:snpID][i]) for i in 1:size(mapData,1)]]]]
+    #genoX = genoTrain[[find(i -> i == j, names(genoTrain))[] for j in [Symbol(mapData[:snpID][i]) for i in 1:size(mapData,1)]]]
+    totLoci = size(genoX[2:end],2) # first col is ID
+    snpInfoFinal = DataFrame(Any, 0, 3)
+    if fixedRegSize==99
+        println("fixedRedSize $fixedRegSize")
+        snpInfoFinal = mapData[:,[:snpID,:snpOrder,:chrID]]
+        accRegion    = length(unique(mapData[:chrID]))
+        elseif fixedRegSize==9999
+            snpInfoFinal = mapData[:,[:snpID,:snpOrder,:chrID]]
+            snpInfoFinal[:,:chrID]  = 1 #was ".=1"
+            accRegion    = 1
+        else
+        for c in 1:chrs
+            thisChr = mapData[mapData[:chrID] .== c,:]
+            totLociChr = size(thisChr,1)
+            TotRegions = ceil(Int,totLociChr/fixedRegSize)
+            accRegion += TotRegions
+            push!(accRegionVec, accRegion)
+            tempGroups = sort(repeat(collect(accRegionVec[c]+1:accRegionVec[c+1]),fixedRegSize))
+            snpInfo = DataFrame(Any, length(tempGroups), 3)
+            snpInfo[1:totLociChr,1] = collect(1:totLociChr)
+            snpInfo[1:totLociChr,2] = thisChr[:snpID]
+            snpInfo[:,3] = tempGroups
+            dropmissing!(snpInfo)
+            snpInfoFinal = vcat(snpInfoFinal,snpInfo)
+            @printf("chr %.0f has %.0f groups \n", c, TotRegions)
+            println(by(snpInfo, :x3, nrow)[:,2])
+        end
+        end  #ends if control flow
+#    print(snpInfoFinal)
+    writecsv("snpInfo",convert(Array,snpInfoFinal))
+    for g in 1:accRegion
+        push!(SNPgroups,searchsorted(snpInfoFinal[:,3], g))
+    end
+    return SNPgroups, genoX
+end
+
+function outputControlSt(onScreen,iter,these2Keep,X,tempBetaVec,μ,varBeta,varE,fixedRegSize)
+    if iter in these2Keep
+        out0 = open(pwd()*"/muOut$fixedRegSize", "a")
+        writecsv(out0, μ)
+        close(out0) 
+        out1 = open(pwd()*"/betaOut$fixedRegSize", "a")
+        writecsv(out1, tempBetaVec')
+        close(out1)
+        out2 = open(pwd()*"/varBetaOut$fixedRegSize", "a")
+        writecsv(out2, varBeta')
+        close(out2)
+        out3 = open(pwd()*"/varEOut$fixedRegSize", "a")
+        writecsv(out3, varE)
+        close(out3)
+        varUhat = var(X*tempBetaVec)
+        out4 = open(pwd()*"/varUhatOut$fixedRegSize", "a")
+        writecsv(out4, varUhat)
+        close(out4)
+        if onScreen==true
+#            varU = var(X*tempBetaVec)
+            @printf("iter %s varUhat %.2f varE %.2f\n", iter, varUhat, varE)
+        elseif onScreen==false
+             @printf("iter %s\n", iter)
+        end
+    end
+end
+
+function fileControlSt(fixedRegSize)
+    for f in ["muOut$fixedRegSize" "betaOut$fixedRegSize" "varBetaOut$fixedRegSize" "varEOut$fixedRegSize" "varUhatOut$fixedRegSize"]
+        if isfile(f)==true
+            rm(f)
+            println("$f removed")
+        end
+    end
+end
+
+function fileControl(nTraits,fixedRegSize)
+    files2Remove = ["muOut$fixedRegSize", "varEOut$fixedRegSize", "covBetaOut$fixedRegSize", "coVarUhatOut$fixedRegSize"]
+    for t in 1:nTraits
+        push!(files2Remove,"beta"*"$t"*"Out$fixedRegSize")
+        push!(files2Remove,"varBeta"*"$t"*"Out$fixedRegSize")
+    end
+    for f in files2Remove
+        if isfile(f)==true
+            rm(f)
+            println("$f removed")
+        end
+    end
+end
+
+function sampleBeta(meanBeta, lhs, varE)
+    return rand(Normal(meanBeta,sqrt(lhs\varE)))
+end
+
+function sampleVarBeta(νS_β,whichLoci,df_β,regionSize)
+    return((νS_β + dot(whichLoci,whichLoci))/rand(Chisq(df_β + regionSize)))
+end
+function sampleVarE(νS_e,yCorVec,df_e,nRecords)
+    return((νS_e + dot(yCorVec,yCorVec))/rand(Chisq(df_e + nRecords)))
+end
+function sampleCovBeta(dfβ, regionSize, Vb , tempBetaMat, theseLoci)
+    Sb = tempBetaMat[:,theseLoci]*tempBetaMat[:,theseLoci]'
+    return rand(InverseWishart(dfβ + regionSize, Vb + Sb))
+end
+
